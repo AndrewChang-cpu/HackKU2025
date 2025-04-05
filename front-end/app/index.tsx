af5c4a0f3d74d2bd5c7b3a79208aae3854a76cd5
Ayman Rahman <ayman2002rahman@gmail.com>
2025-04-05 03:33:24 -0500

    staging changes

--- front-end/app/index.tsx
+++ front-end/app/index.tsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 import { Button, View, Text } from 'react-native-ui-lib';
+import { useRouter } from 'expo-router';
 
 export default function Index() {
+    const router = useRouter();
+
     return (
         <View className="flex-1 bg-cyan-300">
             <View className="flex-1 justify-center items-center">
--- front-end/app/login.tsx
+++ front-end/app/login.tsx
@@ -1,1 +1,20 @@
-import { }
+import { View, Text, Button } from 'react-native-ui-lib';
+
+export default function LoginScreen() {
+    return (
+        <View className="flex-1 bg-cyan-300">
+            <View className="flex-1 justify-center items-center">
+                <Text>Login Screen</Text>
+            </View>
+            <View className='absolute w-full h-full px-[25px] pb-[50px]'>
+                <View className="flex-1 justify-end">
+                    <Button
+                        label={'Get Started'}
+                        enableShadow
+                        animateTo='left'
+                    />
+                </View>
+            </View>
+        </View>
+    );
+}
