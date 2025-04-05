7e42db3d6cba0f4ab57277bc569901e92a0ca41a
Ayman Rahman <ayman2002rahman@gmail.com>
2025-04-05 12:44:24 -0500

    removed merge notes

--- front-end/app/login.tsx
+++ front-end/app/login.tsx
@@ -1,22 +1,13 @@
-<<<<<<< Updated upstream
 import { View, Text, Button } from 'react-native-ui-lib';
-=======
 import { useState, useEffect } from 'react';
 import { View, Text, Button, TextField } from 'react-native-ui-lib';
 //import { tw } from 'tailwind-react-native-classnames';
->>>>>>> Stashed changes
 
 export default function LoginScreen() {
     return (
         <View className="flex-1">
             <View className="flex-1 justify-center items-center">
                 <Text>Login Screen</Text>
-<<<<<<< Updated upstream
-                <Button
-                        label={'Login'}
-                        enableShadow
-                        animateTo='left'
-=======
                 <TextField
                     placeholder="Email"
                     floatingPlaceholder
@@ -27,7 +18,6 @@ export default function LoginScreen() {
                     }}
                     floatingPlaceholderStyle={{ color: '#aaa' }}s 
                     containerStyle={{ marginRight: 8 }}
->>>>>>> Stashed changes
                 />
             </View>
             <View className='absolute w-full h-full px-[25px] pb-[50px]'>
