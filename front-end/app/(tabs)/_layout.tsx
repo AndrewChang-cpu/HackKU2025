f5968e1a62bab795c00767ae10dea789a9541c0c
Ayman Rahman <ayman2002rahman@gmail.com>
2025-04-05 23:00:28 -0500

    fixed tabs layout

--- front-end/app/(tabs)/_layout.tsx
+++ front-end/app/(tabs)/_layout.tsx
@@ -4,56 +4,16 @@ import { Text, View } from "react-native";
 
 export default function TabLayout() {
     return (
-    <Tabs
-        screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarStyle: {
-            backgroundColor: "white",
-            height: 70,
-            paddingBottom: 8,
-            paddingTop: 8,
-            borderTopWidth: 1,
-            borderColor: "#ccc",
-        },
-        tabBarShowLabel: false,
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName = 'home-outline';
-          let label = 'Tab';
-          console.log('ROUTE NAME', route.name)
-
-          if (route.name === 'Home') {
-            iconName = 'home-outline';
-            label = 'Home';
-          } else if (route.name === 'Profile') {
-            iconName = 'person-outline';
-            label = 'Profile';
-          }
-
-          return (
-            <View style={{ alignItems: 'center', justifyContent: 'center', width: 60 }}>
-              <Ionicons
-                name={iconName}
-                size={22}
-                color={focused ? "black" : "#aaa"}
-                />
-                <Text
-                style={{
-                    fontSize: 12,
-                    color: focused ? "black" : "#aaa",
-                    marginTop: 2,
+        <Tabs>
+            <Tabs.Screen 
+                name="index"
+                options={{
+                    title: 'Dashboard',
+                    icon: ''
                 }}
-                numberOfLines={1}
-                ellipsizeMode="tail"
-                >
-                {label}
-                </Text>
-            </View>
-          );
-        },
-      })}
-    >
-      <Tabs.Screen name="Home" />
-      <Tabs.Screen name="Profile" />
-    </Tabs>
+            />
+            <Tabs.Screen name="scanner"/>
+            <Tabs.Screen name="profile"/>
+        </Tabs>
     );
 }
