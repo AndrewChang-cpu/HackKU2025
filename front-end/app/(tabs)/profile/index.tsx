2a7fb9e4b0bf8bf8b2da6ad69ff0324ab9350999
Ayman Rahman <ayman2002rahman@gmail.com>
2025-04-06 05:19:56 -0500

    i hate merge conflicts

--- front-end/app/(tabs)/profile/index.tsx
+++ front-end/app/(tabs)/profile/index.tsx
@@ -8,31 +8,6 @@ import supabase from '@/api/supabaseClient';
 export default function ProfileScreen() {
     const { user } = useAuth();
     const router = useRouter();
-<<<<<<< HEAD
-    return (
-        <SafeAreaView className='flex-1 items-center w-full px-[90px]]'>
-            <Text className='text-[48px]'>Profile</Text>
-            <View className='flex flex-col gap-[50px] w-full bg-white'>
-                <ProfileItem 
-                    title='Medications'
-                    icon='document-text-outline'
-                    onPress={() => {router.push('/(tabs)/profile/AllMedicationsScreen')}}
-                />
-                <ProfileItem 
-                    title='Allergens'
-                    icon='document-text-outline'
-                    onPress={() => {router.push('/profile/allergens')}}
-                />
-                <ProfileItem 
-                    title='Logout'
-                    icon='exit-outline'
-                    onPress={() => {router.push('/')}}
-                />
-            </View>
-        </SafeAreaView>
-    );
-};
-=======
 
     const [name, setName] = useState('');
     useEffect(() => {
@@ -74,4 +49,3 @@ export default function ProfileScreen() {
     </SafeAreaView>
   );
 }
->>>>>>> ayman-dev
